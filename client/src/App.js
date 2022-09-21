866843f9d1a15b3acdb61bc2693200e6d297c695
Lucas Duncan <lucasdonuts@gmail.com>
2022-09-21 15:24:34 -0500

    Initial commit, basic setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import 'bulma/css/bulma.min.css';
+// import './stylesheets/App.css';
+
+function App() {
+  return (
+    <div className='container is-max-widescreen'>
+      <h1 className='title'>DebonairBnB</h1>
+    </div>
+  );
+}
+
+export default App;
