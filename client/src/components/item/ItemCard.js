5f5905fedb9b9da766397d53bbd535fd14150fb6
Lucas Duncan <lucasdonuts@gmail.com>
2022-10-01 17:12:49 -0500

    Added size and category tags to ItemCards. Logo icon in browser tab

--- client/src/components/item/ItemCard.js
+++ client/src/components/item/ItemCard.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const ItemCard = ({ item }) => {
   return (
-    <div class="tile is-3 is-2-fullhd">
+    <div className="tile is-3 is-2-fullhd">
       <Link
         to={`/items/${item.id}`}
         className="tile is-parent is-3 is-2-fullhd item-card"
@@ -17,33 +17,26 @@ const ItemCard = ({ item }) => {
               <img src={item.image} alt={item.name} />
             </figure>
           </div>
-          <div className="has-text-weight-bold">{item.size}</div>
-          {/* <div className="card-content p-0">
-          <div className="content">
-            <p className="is-size-6">{item.name}</p>
+          <p className="is-size-7">
+            <span className="super">$</span>
+            <strong className="is-size-5">{item.price}</strong>/day
+          </p>
+          <div className="field is-grouped is-grouped-centered is-grouped-multiline">
+            {item.category !== "Accessories" && <div className="control">
+              <div className="tags has-addons are-small">
+                <span className="tag is-dark">{item.size}</span>
+              </div>
+            </div>}
+
+            <div className="control">
+              <div className="tags has-addons are-small">
+                <span className="tag is-light">{item.category}</span>
+              </div>
+            </div>
           </div>
-        </div> */}
         </div>
       </Link>
     </div>
-
-    // <Link to={ `/items/${item.id}` } className="is-flex item-card column is-3 is-2-fullhd" style={{minWidth: '190px'}}>
-    //   <div className="is-base-background card">
-    //     <div className="card-header">
-    //       {item.name}
-    //     </div>
-    //     <div className="card-image">
-    //       <figure className="image is-256x256">
-    //         <img src={item.image} alt={item.name} />
-    //       </figure>
-    //     </div>
-    //     {/* <div className="card-content p-0">
-    //       <div className="content">
-    //         <p className="is-size-6">{item.name}</p>
-    //       </div>
-    //     </div> */}
-    //   </div>
-    // </Link>
   );
 };
 
