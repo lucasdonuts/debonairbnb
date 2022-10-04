58b183a7a6cbde1f250de04973e6285dbdbc7324
Lucas Duncan <lucasdonuts@gmail.com>
2022-10-04 15:44:43 -0500

    Footer styles

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,25 +2,27 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="content has-text-centered">
-        <p>
-          <strong>DebonairBnB</strong> by Lucas Duncan
-        </p>
-        <p>
-          <a
-            href="https://github.com/lucasdonuts"
-            target="_blank"
-            className="icon is-size-4 mx-3"
-          >
-            <i className="fab fa-github"></i>
-          </a>
-          <a
-            href="linkedin.com/in/lucaswdunc/"
-            target="_blank"
-            className="icon is-size-4 mx-3"
-          >
-            <i className="fab fa-linkedin"></i>
-          </a>
-        </p>
+        <div className="is-flex is-justify-content-center">
+          <div>
+            <strong>DebonairBnB</strong> by Lucas Duncan
+          </div>
+          <div>
+            <a
+              href="https://github.com/lucasdonuts"
+              target="_blank"
+              className="icon is-size-4 ml-3 mr-2"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+            <a
+              href="linkedin.com/in/lucaswdunc/"
+              target="_blank"
+              className="icon is-size-4 mr-3"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
