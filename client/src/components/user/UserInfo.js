89835c919a3e61106457792c7343dc1910a93755
Lucas Duncan <lucasdonuts@gmail.com>
2022-10-04 15:36:01 -0500

    Footer placement

--- client/src/components/user/UserInfo.js
+++ client/src/components/user/UserInfo.js
@@ -3,16 +3,22 @@ const UserInfo = ({ user }) => {
     <div className="columns is-centered">
       <div className="column is-half">
         <div className="box px-6">
-          <div className="is-flex">
-            <p className="is-size-5 has-text-weight-medium mr-2">Name:</p>
+          <div className="is-flex my-2">
+            <span className="icon is-size-4 mr-3">
+              <i className="fas fa-person"></i>
+            </span>
             <p className="is-size-5 has-text-weight-light">{`${user.first_name} ${user.last_name}`}</p>
           </div>
-          <div className="is-flex">
-            <p className="is-size-5 has-text-weight-medium mr-2">Address:</p>
+          <div className="is-flex my-2">
+            <span className="icon is-size-4 mr-3">
+              <i className="fas fa-house"></i>
+            </span>
             <p className="is-size-5 has-text-weight-light">{user.address}</p>
           </div>
-          <div className="is-flex">
-            <p className="is-size-5 has-text-weight-medium mr-2">Email:</p>
+          <div className="is-flex my-2">
+            <span className="icon is-size-4 mr-3">
+              <i className="fas fa-envelope"></i>
+            </span>
             <p className="is-size-5 has-text-weight-light">{user.email}</p>
           </div>
         </div>
--- client/src/components/user/UserPage.js
+++ client/src/components/user/UserPage.js
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { updateCurrentUser, clearCurrentUser } from "../../reducers/userSlice";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 import EditUserForm from "./EditUserForm";
 import UserInfo from "./UserInfo";
-import Loading from '../Loading';
 
 const UserPage = () => {
-  const { currentUser: user, isLoading } = useSelector((state) => state.user);
+  const { currentUser: user } = useSelector((state) => state.user);
   const [editMode, setEditMode] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
 
@@ -26,18 +25,17 @@ const UserPage = () => {
   };
 
   const deleteAccount = () => {
-    fetch(`/users/${user.id}`, { method: 'DELETE' })
-      .then( () => {
-        dispatch(clearCurrentUser());
-        navigate('/') 
-      })
-  }
+    fetch(`/users/${user.id}`, { method: "DELETE" }).then(() => {
+      dispatch(clearCurrentUser());
+      navigate("/");
+    });
+  };
 
-  document.addEventListener( 'click', (e) => setModalVisible(false))
+  document.addEventListener("click", (e) => setModalVisible(false));
 
   const stopPropagation = (e) => {
     e.stopPropagation();
-  }
+  };
 
   return (
     <div className="content has-text-centered">
@@ -51,46 +49,56 @@ const UserPage = () => {
         <UserInfo user={user} />
       )}
 
-      <button
-        onClick={() => setEditMode(!editMode)}
-        className="button is-primary"
-      >
-        {editMode ? "Cancel" : "Edit Account Details"}
-      </button>
-      <button onClick={ handleDeleteClick } className="button is-danger">
-        Delete Account
-      </button>
+      <div className="field is-grouped is-grouped-centered">
+        <div className="control">
+          <button
+            onClick={() => setEditMode(!editMode)}
+            className="button is-dark"
+          >
+            {editMode ? "Cancel" : "Edit Account Details"}
+          </button>
+        </div>
+        <div className="control">
+          <button onClick={handleDeleteClick} className="button is-danger">
+            Delete Account
+          </button>
+        </div>
+      </div>
       <div
         id="confirm-delete"
-        className={ modalVisible ? "modal is-active" : "modal" }
+        className={modalVisible ? "modal is-active" : "modal"}
       >
         <div className="modal-background"></div>
 
-        <div className="modal-content" onClick={ stopPropagation } >
+        <div className="modal-content" onClick={stopPropagation}>
           <div className="box">
             <p>Are you sure?</p>
             <div className="field is-grouped is-grouped-centered">
               <p className="control">
-                <button onClick={ deleteAccount } className="button is-danger">Delete Account</button>
+                <button onClick={deleteAccount} className="button is-danger">
+                  Delete Account
+                </button>
               </p>
               <p className="control">
-                <button onClick={ () => setModalVisible(false) } className="button">Cancel</button>
+                <button
+                  onClick={() => setModalVisible(false)}
+                  className="button"
+                >
+                  Cancel
+                </button>
               </p>
             </div>
           </div>
         </div>
 
-        <button onClick={ () => setModalVisible(false) } className="modal-close is-large" aria-label="close"></button>
+        <button
+          onClick={() => setModalVisible(false)}
+          className="modal-close is-large"
+          aria-label="close"
+        ></button>
       </div>
     </div>
   );
 };
 
 export default UserPage;
-
-{/* <button onClick={handleDeleteClick} className="button is-danger is-light">
-              Delete Account
-            </button>
-            <button onClick={ () => setModalVisible(false) } className="button is-primary is-light">
-              Cancel
-            </button> */}
