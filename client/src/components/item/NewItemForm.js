5b1e093fa82013eed7b2ba5c00c24dc92aa562ef
Lucas Duncan <lucasdonuts@gmail.com>
2023-02-13 16:56:49 -0600

    Add to wardrobe

--- client/src/components/item/NewItemForm.js
+++ client/src/components/item/NewItemForm.js
@@ -12,7 +12,7 @@ const NewItemForm = () => {
     sex: "",
     category: "",
     size: "",
-    price: null,
+    price: 0,
     image: "",
     owner_id: currentUser.id,
   });
@@ -43,7 +43,7 @@ const NewItemForm = () => {
       body: JSON.stringify(formData),
     }).then((res) => {
       if (res.ok) {
-        res.json().then(console.log("Submitted successfully"));
+        res.json().then(console.log);
       } else {
         res.json().then((data) => setErrors(data.errors));
       }
@@ -54,133 +54,138 @@ const NewItemForm = () => {
     dispatch(getCategories());
   }, []);
 
+  console.log(formData);
+  console.log(errors);
   return (
     <div className="columns is-centered">
       <div className="column is-6 is-4-desktop is-narrow-desktop">
-        <div className="box is-secondary-background">
-          <h1 className="is-brand-font is-size-2 has-text-centered mb-5">
-            Add to Wardrobe
-          </h1>
-          {/* Name */}
-          <div className="field">
-            <label className="label is-small">Item Name</label>
-            <div className="control">
-              <input
-                onChange={handleChange}
-                className="input is-small"
-                type="text"
-                placeholder="Item Name"
-              />
-            </div>
-          </div>
-
-          {/* Sex */}
-          {/* radio buttons */}
-          <div className="field">
-            <div className="control">
-              <label className="radio">
-                <input
-                  onChange={handleChange}
-                  type="radio"
-                  name="sex"
-                  value="Men's"
-                />
-                Men's
-              </label>
-              <label className="radio">
-                <input
-                  onChange={handleChange}
-                  type="radio"
-                  name="sex"
-                  value="Women's"
-                />
-                Women's
-              </label>
-            </div>
-          </div>
-
-          {/* Category, Size, Price */}
-          <div className="field is-grouped is-justify-content-space-around">
-            {/* Category */}
-            {/* dropdown */}
+        <form onSubmit={handleSubmit}>
+          <div className="box is-secondary-background">
+            <h1 className="is-brand-font is-size-2 has-text-centered mb-5">
+              Add to Wardrobe
+            </h1>
+            {/* Name */}
             <div className="field">
-              <label className="label is-small">Category</label>
-              <div className="control">
-                <div className="select is-small">
-                  <select
-                    name="category"
-                    onChange={handleChange}
-                    value={formData.category}
-                  >
-                    <option value="">--Select Category--</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-
-            {/* Size */}
-            {/* dropdown */}
-            <div className="field">
-              <label className="label is-small">Size</label>
-              <div className="control">
-                <div className="select is-small">
-                  <select
-                    name="size"
-                    onChange={handleChange}
-                    value={formData.size}
-                  >
-                    <option value="">--Select Size--</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-
-            {/* Price */}
-            {/* number field */}
-            <div className="field">
-              <label className="label is-small">Price</label>
+              <label className="label is-small">Item Name</label>
               <div className="control">
                 <input
                   onChange={handleChange}
                   className="input is-small"
-                  type="number"
-                  value={formData.price}
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  placeholder="Item Name"
                 />
               </div>
             </div>
-          </div>
 
-          {/* Image */}
-          {/* text input */}
-          <div className="field">
-            <label className="label is-small is-small">Image URL</label>
-            <div className="control">
-              <input
-                className="input is-small"
-                type="text"
-                placeholder="Image URL"
-                value={formData.image}
-              />
+            {/* Sex */}
+            {/* radio buttons */}
+            <div className="field">
+              <div className="control">
+                <label className="radio">
+                  <input
+                    onChange={handleChange}
+                    type="radio"
+                    name="sex"
+                    value="Men's"
+                  />
+                  Men's
+                </label>
+                <label className="radio">
+                  <input
+                    onChange={handleChange}
+                    type="radio"
+                    name="sex"
+                    value="Women's"
+                  />
+                  Women's
+                </label>
+              </div>
             </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <label className="checkbox">
-                <input type="checkbox" />I agree to the{" "}
-                <a href="#">terms and conditions</a>
-              </label>
-            </div>
-          </div>
 
-          <div className="field is-grouped is-grouped-centered">
-            <div className="control">
-              <button className="button is-link">Submit</button>
+            {/* Category, Size, Price */}
+            <div className="field is-grouped is-justify-content-space-around">
+              {/* Category */}
+              {/* dropdown */}
+              <div className="field">
+                <label className="label is-small">Category</label>
+                <div className="control">
+                  <div className="select is-small">
+                    <select
+                      onChange={handleChange}
+                      name="category"
+                      value={formData.category}
+                    >
+                      <option value="">--Select Category--</option>
+                      <option value="Shirts">Shirts</option>
+                      <option value="Pants">Pants</option>
+                      <option value="Shoes">Shoes</option>
+                      <option value="Accessories">Accessories</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+
+              {/* Size */}
+              {/* dropdown */}
+              <div className="field">
+                <label className="label is-small">Size</label>
+                <div className="control">
+                  <div className="select is-small">
+                    <select
+                      onChange={handleChange}
+                      name="size"
+                      value={formData.size}
+                    >
+                      <option value="">--Select Size--</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+
+              {/* Price */}
+              {/* number field */}
+              <div className="field">
+                <label className="label is-small">Price</label>
+                <div className="control">
+                  <input
+                    onChange={handleChange}
+                    name="price"
+                    className="input is-small"
+                    type="number"
+                    value={formData.price}
+                  />
+                </div>
+              </div>
             </div>
-            <div className="control">
-              <button className="button is-link is-light">Cancel</button>
+
+            {/* Image */}
+            {/* text input */}
+            <div className="field">
+              <label className="label is-small is-small">Image URL</label>
+              <div className="control">
+                <input
+                  onChange={handleChange}
+                  name="image"
+                  className="input is-small"
+                  type="text"
+                  placeholder="Image URL"
+                  value={formData.image}
+                />
+              </div>
+            </div>
+
+            <div className="field is-grouped is-grouped-centered">
+              <div className="control">
+                <button className="button is-link">Submit</button>
+              </div>
+              <div className="control">
+                <button className="button is-link is-light">Cancel</button>
+              </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
